4b5bb729ae4984a09e02b397d885e8e65a022630
unknown <13240327919@163.com>
2016-07-10 17:11:49 +0800

    Signed-off-by: unknown <13240327919@163.com>

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -69,3 +69,10 @@ Route::get('/jGetOwnerTeams', 'TeamController@getMyTeams');
 Route::post('/jStudentCreateTeam', 'TeamController@postJsonCreateTeam');
 Route::post('/jStudentApplyTeam', 'TeamController@getApplyJoinTeam');
 //Team相关模块 -->
+
+
+#SA 相关操作
+Route::post('/admin/getEAInfo', 'SAController@getEAInfo'); //获取所有EA信息列表
+// Route::post('/admin/editEAInfo',''); //修改EA信息
+// Route::post('/admin/delEAInfo','');  //删除EA信息
+// Route::post('/admin/addEAInfo', ''); //添加EA信息
